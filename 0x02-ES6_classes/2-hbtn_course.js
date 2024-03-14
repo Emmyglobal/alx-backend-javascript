c8d455e6ab2adbc3961c81f539c61352d1128dba
Nwafor Ugochukwu Emmanuel <nwaforugochukwu21@gmail.com>
2024-03-14 05:34:46 +0000

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,32 +1,39 @@
-class HolbertonCourse {
-	constructor(name, length, students) {
-		this._name = typeof name === 'string' ? name : '';
-		this._length = typeof length === 'number' ? length : 0;
-		this._students = Array.isArray(students) ? students : [];
-	}
-	get name() {
-		return this._name;
-	}
-	set name(newName) {
-		if (typeof newName === 'string') {
-			this._name = newName;
-		}
-	}
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    if (!Number.isInteger(length)) throw new TypeError('length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
 
-	get length() {
-		return this._length;
-	}
-	set length(newLength) {
-		if (typeof newLength === 'number') {
-			this._length = newLength;
-		}
-	}
-	get students() {
-		return this._students;
-	}
-	set students(newStudents) {
-		if (Array.isArray(newStudents)) {
-			this._students = newStudents;
-		}
-	}
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  } // Object Creation
+
+  // Getter and Setter Function
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') throw new TypeError('name must be a string');
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (!Number.isInteger(val)) throw new TypeError('length must be a number');
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(val) {
+    if (!Array.isArray(val)) throw new TypeError('students type must be an Array');
+    this._students = val;
+  }
 }
