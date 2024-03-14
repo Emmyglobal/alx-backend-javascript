e71f2ffd4fa802b7e22de033108b65ec29716517
Nwafor Ugochukwu Emmanuel <nwaforugochukwu21@gmail.com>
2024-03-14 09:02:06 +0000

    Methods, static methods, computed methods names..... MONEY

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -4,7 +4,6 @@ export default class Currency {
 		this._name = name;
 	}
 
-	//Getter and setter for 'code'
 	get code() {
 		return this._code;
 	}
@@ -14,8 +13,6 @@ export default class Currency {
 			this._code = newwCode;
 		}
 	}
-
-	// Getter and setter for 'name'
 	get name() {
 		return this._name;
 	}
