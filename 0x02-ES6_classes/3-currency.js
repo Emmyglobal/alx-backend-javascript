4f468a055bc22d23c259b3132caf71622e9dcc05
Nwafor Ugochukwu Emmanuel <nwaforugochukwu21@gmail.com>
2024-03-15 22:28:02 +0000

    fixed

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,18 +3,23 @@ export default class Currency {
 		this._code = code;
 		this._name = name;
 	}
+
 	get code() {
 		return this._code;
 	}
+
 	set name(newCode) {
 		this._code = newwCode;
 	}
+
 	get name() {
 		return this._name;
 	}
+
 	set name(newName) {
 		this._name = newName;
-	}	
+	}
+
 	displayFullCurrency() {
 		return `${this.name} (${this.code})`;
 	}
