10623748c93019efaefbed9ef613a02cc7ace8a8
Nwafor Ugochukwu Emmanuel <nwaforugochukwu21@gmail.com>
2024-04-29 20:30:25 +0000

    hot fix

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,8 @@
  * Prints in STDOUT the string `args`
  * @param {String} args
  */
-
 function displayMessage(args) {
-	console.log(args);
+  console.log(args);
 }
+
 module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,63 @@
+const { readFileSync } = require('fs');
+
+/**
+ * Read the database file provided in `path` synchronously
+ * @param {Path} path
+ */
+function countStudents(path) {
+  const students = {}; // initialize empty dict
+  const fields = {}; // initialize empty dict
+  let length = 0; // length counter
+  try {
+    // read the file synchronously
+    const content = readFileSync(path, 'utf-8');
+    // console.log(content);
+    // split the file by new-lines and save as list
+    let lines = content.toString().split('\n');
+    lines = lines.slice(1); // delete the csv headers
+    // console.log(lines);
+    for (let i = 0; i < lines.length; i += 1) { // loop thru the list
+      if (lines[i]) { // if elememt is valid
+        const field = lines[i].toString().split(',');
+        if (field[0] === '') { // chk for empty lines
+          // console.log("All good");
+          length += 0;
+        } else {
+          // console.log(field[0]);
+          length += 1;
+
+          // console.log(field);
+          // chk if the the 4th elem in list `field` exists in students dict
+          if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+            //  append the student's 1st name to the list
+            students[field[3]].push(field[0]);
+          } else {
+            // create a key of the 4th elem with a list of firstnames as value
+            students[field[3]] = [field[0]];
+          }
+        } // store the num of students per field
+        // increment if key exists already
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          if (field[3] !== '') {
+            fields[field[3]] += 1;
+          }
+        } else if (field[3] !== '') { // create k:v init to 1
+          fields[field[3]] = 1;
+        }
+      }
+    }
+    // console.log(fields);
+    console.log(`Number of students: ${length}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        // console.log(`Anoda key ${key}`);
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw Error('Cannot load the database');
+    // console.log(error);
+  }
+}
+
+module.exports = countStudents;
