7a3ab5909a1ed7fb3ca4645fe2f36ba1666bc30a
Emmyglobal <nwaforugochukwu21@gmail.com>
2024-06-25 02:01:41 +0000

    pushed

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,20 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('../0-calcul');
 
-describe('calculateNumber', () => {
-  it('1 + 3 should return 4', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-  });
-
-  it('1 + 3.7 should return 5', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
-  });
-
-  it('1.5 + 3.7 should return 6', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('-1.5 + 3.7 should return 2', () => {
-    assert.equal(calculateNumber(-1.5, 3.7), 3);
-  });
-
-  it('-1.5 + -3.7 should return -5', () => {
-    assert.equal(calculateNumber(-1.5, -3.7), -5);
-  });
+describe('calculateNumber', function() {
+	it('should return 4 when a is 1.4 and b is 2.5', () => {
+		assert.strictEqual(calculateNumber(1.4, 2.5), 4);
+	});
+	it('should return 5 when a is 1.5 and b is 2.5', () => {
+		assert.strictEqual(calculateNumber(1.5, 2.5), 5);
+	});
+	it('should return 0 when a is 0 and b is 0', () => {
+		assert.strictEqual(calculateNumber(0, 0), 0);
+	});
+	it('should return -2 when a is -1.4 and b is -0.5', () => {
+		assert.strictEqual(calculateNumber(-1.4, -0.5), -2);
+	});
+	it('should return -3 when a is -1.5 and b is -1.5', () => {
+		assert.strictEqual(calculateNumber(-1.5, -1.5), -3);
+	});
 });
