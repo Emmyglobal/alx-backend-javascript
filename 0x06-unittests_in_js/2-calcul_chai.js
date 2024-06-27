791f28674026401f79cc47c4edec879de5082bff
Emmyglobal <nwaforugochukwu21@gmail.com>
2024-06-27 08:53:18 +0000

    All test passed after using expect from chai

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,18 +1,18 @@
 function calculateNumber(type, a, b) {
-    const rounded_a = Math.round(a);
-    const rounded_b = Math.round(b);
+    const x = Math.round(a);
+    const y = Math.round(b);
     
     if (type === 'SUM') {
-      return rounded_a + rounded_b;
+      return x + y;
     }
     if (type === 'SUBTRACT') {
-      return rounded_a - rounded_b;
+      return x - y;
     }
     if (type === 'DIVIDE') {
-      if (rounded_b === 0) {
+      if (y === 0) {
         return 'Error';
       }
-      return rounded_a / rounded_b;
+      return x / y;
     }
   }
   
